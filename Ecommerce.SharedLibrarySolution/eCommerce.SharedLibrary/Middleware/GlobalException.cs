09e66b8b65b181c48dfa0ad7b297d41d315f2228
crissiiu <crissiiu.dev@gmail.com>
2026-06-15 17:33:13 +0700

    Common Library

---
+++ Ecommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,80 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "sorry, internal server error occurred. Kindly try again";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+                //check if Response is To many request //429 status code.
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many request made.";
+                    statusCode = (int)StatusCodes.Status429TooManyRequests;
+                    await ModifyHeaderAsync(context, title, message, statusCode);
+                }
+
+                //check if Response is UnAuthorized //401 status code.
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access.";
+                    statusCode = (int)StatusCodes.Status401Unauthorized;
+                    await ModifyHeaderAsync(context, title, message, statusCode);
+                }
+
+                //check if Response is Forbidden //403 status code.
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of Access";
+                    message = "You are not allowed/required to access.";
+                    statusCode = (int)StatusCodes.Status403Forbidden;
+                    await ModifyHeaderAsync(context, title, message, statusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log Original Exceptions / File, Console, Debugger
+                LogException.LogExceptions(ex);
+
+                //check if Exception is Timeout // 408 request time 
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Out of Time";
+                    message = "Request timeout... try again";
+                    statusCode = (int)StatusCodes.Status408RequestTimeout;
+                }
+
+                //if exception is caught
+                //if none of the exceptions then do the default
+
+                await ModifyHeaderAsync(context, title, message, statusCode);
+            }
+        }
+
+        private async Task ModifyHeaderAsync(HttpContext context, string title, string message, int statusCode)
+        {
+            //display scary-free message to client
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Detail = message,
+                Status = statusCode,
+                Title = title
+            }), CancellationToken.None);
+            return;
+        }
+    }
+}
---
+++ Ecommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Extract specific header from the request 
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            //NULL means, the request is not coming from the api gateway //503 ServiceUnvailable
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, service is unvailable");
+                return;
+            }
+            else 
+            {
+                await next(context);
+            }
+
+        }
+    }
+}
